b417b11170a4a4ca3e91c9629f2f5a9c4ad2dbbb
Daniel Cho <chodaniel89@gmail.com>
2022-10-18 19:00:16 +0000

    Work in Progress.

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -1,81 +1,84 @@
-var orderHistory = [
-  {
-    orderIndex: [
-      {
-        type: {
-          type: 'book',
-          title: 'Javascript for impatient programmers',
-          author: 'Rauschmayer, Dr. Axel'
-        },
-        dateOrdered: '8-4-20',
-        dateDelivered: '8-8-20',
-        returnWindow: '9-7-20',
-        price: 31.55,
-        shipto: 'JS Masher'
-      }
-    ]
-  },
-  {
-    orderIndex: [
-      {
-        type: {
-          type: 'book',
-          title: 'The Timeless Way of Building',
-          author: 'Alexander, Christopher'
-        },
-        dateOrdered: '7-20-20',
-        dateDelivered: '7-20-20',
-        returnWindow: '8-19-20',
-        price: 41.33,
-        shipto: 'JS Masher'
-      }
-    ]
-  },
-  {
-    orderIndex: [
-      {
-        type: {
-          type: 'adapter',
-          title: 'Gamecube Controller Adapter.',
-          author: null
-        },
-        dateOrdered: '7-4-20',
-        dateDelivered: '7-7-20',
-        returnWindow: '8-5-20',
-        price: 15.98,
-        shipto: 'JS Masher'
-      }
-    ]
-  },
-  {
-    orderIndex: [
-      {
-        type: {
-          type: 'controller',
-          title: 'Gamecube Controller',
-          author: null
-        },
-        dateOrdered: '7-3-20',
-        dateDelivered: '7-5-20',
-        returnWindow: '8-4-20',
-        price: 94.95,
-        shipto: 'JS Masher'
-      }
-    ],
-    orderIndex2:
-     [
-       {
-         type: {
-           type: 'book',
-           title: 'The Art of Sql',
-           author: 'Faroult, Stephane'
-         },
-         dateOrdered: '7-3-20',
-         dateDelivered: '7-5-20',
-         returnWindow: '8-4-20',
-         price: 33.99,
-         shipto: 'JS Masher'
-       }
-     ]
-  }
-];
+// var orderHistory = [
+//   {
+//     Items: [
+//       {
+//         type: 'book',
+//         title: 'Javascript for impatient programmers',
+//         author: 'Rauschmayer, Dr. Axel',
+//         image: 'URL',
+//         returnWindow: '09-07-20',
+//         price: 31.55
+//       }
+//     ],
+//     orderNumber: '114-3941689-8772232',
+//     dateOrdered: '08-04-20',
+//     dateDelivered: '08-08-20',
+//     total: 34.00,
+//     shipto: 'JS Masher'
+//   },
+//   {
+//     Items: [
+//       {
+//         type: 'book',
+//         title: 'The Timeless Way of Building',
+//         author: 'Alexander, Christopher',
+//         image: 'URL',
+//         returnWindow:
+//       }
+//     ],
+//     dateOrdered: '7-20-20',
+//     dateDelivered: '7-20-20',
+//     returnWindow: '8-19-20',
+//     price: 41.33,
+//     shipto: 'JS Masher'
+//       }
+//     ]
+//   },
+//   {
+//     orderIndex: [
+//       {
+//         type: {
+//           type: 'adapter',
+//           title: 'Gamecube Controller Adapter.',
+//           author: null
+//         },
+//         dateOrdered: '7-4-20',
+//         dateDelivered: '7-7-20',
+//         returnWindow: '8-5-20',
+//         price: 15.98,
+//         shipto: 'JS Masher'
+//       }
+//     ]
+//   },
+//   {
+//     orderIndex: [
+//       {
+//         type: {
+//           type: 'controller',
+//           title: 'Gamecube Controller',
+//           author: null
+//         },
+//         dateOrdered: '7-3-20',
+//         dateDelivered: '7-5-20',
+//         returnWindow: '8-4-20',
+//         price: 94.95,
+//         shipto: 'JS Masher'
+//       }
+//     ],
+//     orderIndex2:
+//      [
+//        {
+//          type: {
+//            type: 'book',
+//            title: 'The Art of Sql',
+//            author: 'Faroult, Stephane'
+//          },
+//          dateOrdered: '7-3-20',
+//          dateDelivered: '7-5-20',
+//          returnWindow: '8-4-20',
+//          price: 33.99,
+//          shipto: 'JS Masher'
+//        }
+//      ]
+//   }
+// ];
