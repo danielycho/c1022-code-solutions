1ffe09782481d11df204591cd7478f70f160401b
Daniel Cho <chodaniel89@gmail.com>
2022-10-19 19:11:59 +0000

    partially done

---
+++ javascript-if/main.js
@@ -0,0 +1,40 @@
+/* exported isUnderFive,
+            isEven,
+            startsWithJ,
+            isOldEnoughToDrink,
+            isOldEnoughToDrive,
+            isOldEnoughToDrinkAndDrive,
+            categorizeAcidity,
+            introduceWarnerBro
+ */
+function isUnderFive(number) {
+  return number < 5;
+}
+
+function isEven(number) {
+  if (number % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function startsWithJ(string) {
+  if (string[0] === 'J') {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// var bart = {
+//   name: "Bart",
+//   age: 10
+// }
+// function isOldEnoughToDrink(person) {
+//   if person[age] =< 21 {
+//     return true;
+//   } else {
+//       return false;
+//     }
+// }
