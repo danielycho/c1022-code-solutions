356a1ed449157bce0cdd9a9f727933675048c19b
Daniel Cho <chodaniel89@gmail.com>
2022-10-20 23:31:33 +0000

    Completed javascript-loops-practice exercise.

--- javascript-loops-practice/odd-or-even.js
+++ javascript-loops-practice/odd-or-even.js
@@ -1,1 +1,13 @@
 /* exported oddOrEven */
+function oddOrEven(numbers) {
+  var array = [];
+  for (var i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 0) {
+      array.push('even');
+    }
+    if (numbers[i] % 2 !== 0) {
+      array.push('odd');
+    }
+  }
+  return array;
+}
