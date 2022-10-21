566c19e491ceb58e45882716a4c129fa7dabebbb
Daniel Cho <chodaniel89@gmail.com>
2022-10-21 21:04:11 +0000

    Completed string-manipulation-1 exercise.

--- string-manipulation-1/src/reverse-word.js
+++ string-manipulation-1/src/reverse-word.js
@@ -1,1 +1,12 @@
 /* exported reverseWord */
+// create a variable string
+// start at end and move backwards by setting initialization from word.length - 1
+// use for loop to iterate through the word from the end going down.
+// return string
+function reverseWord(word) {
+  var string = '';
+  for (var i = word.length - 1; i >= 0; i--) {
+    string = string + word[i];
+  }
+  return string;
+}
