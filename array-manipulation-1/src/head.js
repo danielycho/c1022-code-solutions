3eb44cd0d5ae3000a4fad3eee8a2aa1b793bccda
Daniel Cho <chodaniel89@gmail.com>
2022-10-21 21:24:27 +0000

    Work in progress.

---
+++ array-manipulation-1/src/head.js
@@ -0,0 +1,5 @@
+/* exported head */
+// return first index of an array
+function head(array) {
+  return array[0];
+}
---
+++ array-manipulation-1/src/last.js
@@ -0,0 +1,5 @@
+/* exported last */
+// return last element of an array
+function last(array) {
+  return array[array.length - 1];
+}
