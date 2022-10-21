11c9b474c67b273a4bc271f584b31f172a7d1c9d
Daniel Cho <chodaniel89@gmail.com>
2022-10-21 18:35:28 +0000

    Fixed filter-out-string.

--- javascript-loops-practice/filter-out-strings.js
+++ javascript-loops-practice/filter-out-strings.js
@@ -2,7 +2,7 @@
 function filterOutStrings(values) {
   var array = [];
   for (var i = 0; i < values.length; i++) {
-    if (typeof values[i] === 'number' || typeof values[i] === 'boolean' || typeof values[i] === 'object') {
+    if (typeof values[i] !== 'string') {
       array.push(values[i]);
     }
   } return array;
