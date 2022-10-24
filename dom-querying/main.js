efd597517d89ed30c9c505d3e23698d7d3a5d54f
Daniel Cho <chodaniel89@gmail.com>
2022-10-24 17:12:34 +0000

    Completed dom querying exercise

---
+++ dom-querying/main.js
@@ -0,0 +1,18 @@
+console.log('hello, world');
+var elemOne = document.querySelector('h1');
+console.log('elemOne value:', elemOne);
+console.dir(elemOne);
+
+var valExplanation = document.querySelector('#explanation');
+console.log('value of explanation', valExplanation);
+console.dir(valExplanation);
+
+var valHint = document.querySelector('.hint');
+console.log('value of hint', valHint);
+console.dir(valHint);
+
+var allP = document.querySelectorAll('p');
+console.log('value of allP', allP);
+
+var exLink = document.querySelectorAll('.example-link');
+console.log('value of exLink', exLink);
