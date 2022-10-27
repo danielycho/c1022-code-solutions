055db53fe6379f790c24c6762142a1f4ca0f17cf
Daniel Cho <chodaniel89@gmail.com>
2022-10-27 23:24:50 +0000

    Work in progress

---
+++ array-manipulation-2/src/take-right.js
@@ -0,0 +1,8 @@
+/* exported takeRight */
+// function takeRight(array, count) {
+//   var newArray = [];
+//   for (var i = array.length - count; i < array.length; i++) {
+//     newArray.push[i];
+//   }
+//   return newArray;
+// }
