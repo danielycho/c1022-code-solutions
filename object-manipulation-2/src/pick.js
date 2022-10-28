19763648315972c66618afb30730124f410a4136
Daniel Cho <chodaniel89@gmail.com>
2022-10-28 23:58:59 +0000

    Need to Stew

--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -1,1 +1,16 @@
 /* exported pick */
+// create empty newObj
+// use for in loop to get key and value.
+// compare using includes method. use conditional keys.includes(key)
+// if they match they should be put in the newObj
+//
+
+function pick(source, keys) {
+  var newObj = {};
+  for (var key in source) {
+    if (keys.includes(key)) {
+      newObj[key] = source[key];
+    }
+  }
+  return newObj;
+}
