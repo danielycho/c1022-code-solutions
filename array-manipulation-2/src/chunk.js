eef41f45f4a46539bbd361d1ebdbc011dcf5fb55
Daniel Cho <chodaniel89@gmail.com>
2022-10-28 22:41:38 +0000

    array-manipulation-2 chunk done.

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,1 +1,20 @@
 /* exported chunk */
+// Create a new array
+// if size > array.length return array.
+// use a for loop to loop through the array.
+// using .slice(start, end) noticed that place to cut off is at index = size
+// to make the for loop loop once to cut that I need to make it increment by size.
+// push the slice array into new array
+// return new array.
+
+function chunk(array, size) {
+  var finArray = [];
+  if (size > array.length) {
+    return finArray;
+  }
+  for (var i = 0; i < array.length; i = i + size) {
+    var slicedArray = array.slice(i, i + size);
+    finArray.push(slicedArray);
+  }
+  return finArray;
+}
