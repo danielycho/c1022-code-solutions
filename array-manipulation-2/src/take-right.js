569b1dcf5bab0d0a1b48998dfd0918d0512f9c61
Daniel Cho <chodaniel89@gmail.com>
2022-10-28 02:59:55 +0000

    Completed required array exercises. May come back for chunk(array, size).

--- array-manipulation-2/src/take-right.js
+++ array-manipulation-2/src/take-right.js
@@ -1,8 +1,17 @@
 /* exported takeRight */
-// function takeRight(array, count) {
-//   var newArray = [];
-//   for (var i = array.length - count; i < array.length; i++) {
-//     newArray.push[i];
-//   }
-//   return newArray;
-// }
+// create new array
+// set a conditional that if count > array.length just return array
+// create a loop.  The loop should start at array.length - count because we're taking
+// from the right.
+// push into new array and return new array.
+
+function takeRight(array, count) {
+  var newArray = [];
+  if (count > array.length) {
+    return array;
+  }
+  for (var i = array.length - count; i < array.length; i++) {
+    newArray.push(array[i]);
+  }
+  return newArray;
+}
