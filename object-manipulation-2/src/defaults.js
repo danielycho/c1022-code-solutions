3021895443611a434baed2b569a5d7ac9d405f5f
Daniel Cho <chodaniel89@gmail.com>
2022-10-29 19:20:09 +0000

    Completed object-manipulation-2.

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,1 +1,12 @@
 /* exported defaults */
+// for in loop through the key in source
+// if the target doesn't have undefined property for key,
+// copy over source[key] value to target[key];
+
+function defaults(target, source) {
+  for (var key in source) {
+    if (target[key] === undefined) {
+      target[key] = source[key];
+    }
+  }
+}
--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -1,1 +1,15 @@
 /* exported omit */
+// Uses same concept of pick.js, but putting a !keys.includes(key) in the conditional
+// gives opposite effect.
+
+function omit(source, keys) {
+  var newObj = {};
+  for (var key in source) {
+    if (!keys.includes(key)) {
+      if (source[key] !== undefined) {
+        newObj[key] = source[key];
+      }
+    }
+  }
+  return newObj;
+}
