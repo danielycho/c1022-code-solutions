38e632e396b723992f91596eb2b7ccc3a3e7a2d8
Daniel Cho <chodaniel89@gmail.com>
2022-10-31 00:46:01 +0000

    javascript-forms: reviewed and changed the eventlistener a bit for clarification.

--- javascript-forms/form-values.js
+++ javascript-forms/form-values.js
@@ -1,17 +1,26 @@
 var contactForm = document.querySelector('#contact-form');
 
-function handleSubmit(event) {
-  event.preventDefault();
-  var name = contactForm.elements.name.value;
-  var email = contactForm.elements.email.value;
-  var message = contactForm.elements.message.value;
-  var messageData = {
-    name,
-    email,
-    message
-  };
-  console.log(messageData);
-  contactForm.reset();
-}
+// function handleSubmit(event) {
+//   event.preventDefault();
+//   var name = contactForm.elements.name.value;
+//   var email = contactForm.elements.email.value;
+//   var message = contactForm.elements.message.value;
+//   var messageData = {
+//     name,
+//     email,
+//     message
+//   };
+//   console.log(messageData);
+//   contactForm.reset();
+// }
 
 contactForm.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  var messageData = {};
+  messageData.name = contactForm.elements.name.value;
+  messageData.email = contactForm.elements.email.value;
+  messageData.message = contactForm.elements.message.value;
+
+}
